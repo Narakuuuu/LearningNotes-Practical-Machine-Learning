fc4d476acc2f6c3b1303bc1ef87466360a545516
xurongxing <xurongxing@bytedance.com>
2024-08-07 22:41:51 +0800

    --study=学习笔记--2.1 探索性数据分析（ing）

--- demo/2_1_exploratory_data_analysis_for_house_sales/.ipynb_checkpoints/demo-checkpoint.py
+++ demo/2_1_exploratory_data_analysis_for_house_sales/.ipynb_checkpoints/demo-checkpoint.py
@@ -1,3 +1,4 @@
+# jupyter
 # numpy: python里面做数据分析最常见的包
 # pandas: 处理表的工具包
 # matplotlib.pyplot/seaborn: 画图工具
@@ -17,19 +18,8 @@ display.set_matplotlib_formats('svg')
 # import matplotlib_inline
 # matplotlib_inline.backend_inline.set_matplotlib_formats('svg')
 
-# 读数据集文件，输出样本量（行）和特征数（列）
 data = pd.read_feather('data/house_sales.ftr')
+
 data.shape
 
-# 特征类型
 data.head()
-
-# 计算每列的缺失值数量。
-null_sum = data.isnull().sum()
-# 判断每列的缺失值数量是否少于总行数的 30%,根据上述条件筛选出需要保留的列名。
-data.columns[null_sum < len(data) * 0.3] # colume will keep
-# 判断每列的缺失值数量是否超过总行数的 30%,根据上述条件筛选出需要删除的列名,并删除
-data.drop(columns=data.columns[null_sum > len(data) * 0.3], inplace=True)
-
-# 特征（列）类型
-data.dtypes
