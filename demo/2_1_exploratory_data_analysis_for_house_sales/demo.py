4219c469fadf4d1a472fd72533d0ea133633a7d3
xurongxing <xurongxing@bytedance.com>
2024-08-08 11:12:50 +0800

    --content=test

---
+++ demo/2_1_exploratory_data_analysis_for_house_sales/demo.py
@@ -0,0 +1,10 @@
+# numpy: python里面做数据分析最常见的包
+# pandas: 处理表的工具包
+# matplotlib.pyplot/seaborn: 画图工具
+# 数据处理+可视化
+# -------------------------------------
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pyarrow
